77db0f4522e5682e46abc5f00883be138c012a0a
ashubajara <banjaraashutosh@gmail.com>
2021-07-08 15:53:57 +0545

    added construct grid function and draw snake function

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+// JS For Snake Game
 
-const grid = document.getElementById("grid");
-const square = document.getElementsByClassName("");
 const root = document.documentElement;
 
 
 let snake = {
-    currentPos: [],
+    currentPos: [0,1,2],
     direction: "w" // Can be w - up, s - down, a - left or d - right
 }
 
+let grid = {
+    gridSquares: [],
+    gridEl: document.getElementById("grid"),
+    width: 16
+}
+
 // Set the grid size based on screen size, supports displays down to 256px wide
-function setGridSize(gridSize){
+function setGridSize(){
     let screenWidth = screen.width;
 
     if (screenWidth >= 512){
@@ -23,11 +28,26 @@ function setGridSize(gridSize){
         root.style.setProperty("--gridWidth", "256px");
     }
     // Border added to stop a line appearing across screen at START
-    grid.style.setProperty("border", "2px #525252 solid");
+    grid.gridEl.style.setProperty("border", "2px #525252 solid");
 }
 
+// Construct the grid using 16x16 squares.
 function constructGrid(){
+    for (let i = 0; i < grid.width*grid.width; i++){
+        // create a 'square', give it the class 'square' and add it to the grid
+        const square = document.createElement('div');
+        square.classList.add("square");
+        grid.gridEl.appendChild(square);
+        grid.gridSquares.push(square);
+    }
+}
 
+function drawSnake(){
+    for (let i = 0; i < snake.currentPos.length; i++){
+        grid.gridSquares[i].classList.add("snake");
+    }
 }
 
 setGridSize();
+constructGrid();
+drawSnake();
