8644718efda7db1b956e7e57c17316de14cdcc43
ashubajara <banjaraashutosh@gmail.com>
2021-07-10 21:44:39 +0545

    added generate apple function, fixed bug in collison detection

--- script.js
+++ script.js
@@ -62,23 +62,6 @@ function drawSnake(){
     }
 }
 
-
-// Move the snake 1 unit in its current direction
-// Stop snake movement if collision is detected
-function moveSnake(){
-    if (checkCollision()){
-        return clearInterval(timerId);
-    }
-    // Remove the last element of the snake
-    const tail = snake.currentPos.pop();
-    grid.gridSquares[tail].classList.remove("snake");
-
-    // Add it to the front of the Snake
-    snake.currentPos.unshift(snake.currentPos[0] + snake.direction)
-    grid.gridSquares[snake.currentPos[0]].classList.add("snake");
-}
-
-
 // Check for collisions with the wall or itself (game over condition)
 // returns 1 if collision has been detected, otherwise returns 0
 function checkCollision(timerId){
@@ -95,25 +78,63 @@ function checkCollision(timerId){
     return 0;
 }
 
+// Calculate next iteration of game
+// Generate and Draw Apple at the start
+// Move the snake 1 unit in its current direction
+// Stop snake movement if collision is detected
+function iterateGame(){
+    if (checkCollision()){
+        return clearInterval(timerId);
+    }
+    //drawApple();
+    // Remove the last element of the snake
+    const tail = snake.currentPos.pop();
+    grid.gridSquares[tail].classList.remove("snake");
+
+    // Add it to the front of the Snake
+    snake.currentPos.unshift(snake.currentPos[0] + snake.direction)
+    grid.gridSquares[snake.currentPos[0]].classList.add("snake");
+}
+
+// Generate an apple in a random position not held by the snake and return
+// its position
+function generateApple(){
+    let randNum = snake.currentPos[0];
+    while(snake.currentPos.includes(randNum)){
+        randNum = Math.floor(Math.random()*(grid.width*grid.width));
+    }
+    return randNum;
+}
+
+// Call generate apple, then draw the apple on the board
+function drawApple(){
+    let appleIndex = generateApple();
+    grid.gridSquares[appleIndex].classList.add("apple");
+}
+
 
 // == EVENT LISTENERS ==
 
 // Add event listener/function to control the snake
 document.addEventListener('keydown', function(event){
-    if (event.code === "KeyW" || event.code === "ArrowUp") {
+    if ((event.code === "KeyW" || event.code === "ArrowUp") &&
+    (snake.direction != grid.width)) {
         snake.direction = -grid.width;
-    } else if (event.code === "KeyS" || event.code === "ArrowDown") {
+    } else if ((event.code === "KeyS" || event.code === "ArrowDown") &&
+    (snake.direction != -grid.width)) {
         snake.direction = grid.width;
-    } else if (event.code === "KeyA" || event.code === "ArrowLeft") {
+    } else if ((event.code === "KeyA" || event.code === "ArrowLeft") &&
+    (snake.direction != 1)) {
         snake.direction = -1;
-    } else if (event.code === "KeyD" || event.code === "ArrowRight") {
+    } else if ((event.code === "KeyD" || event.code === "ArrowRight") &&
+    (snake.direction != -1)) {
         snake.direction = 1;
     }
 })
 
 // Start button
 startBtn.addEventListener('click', function(){
-    timerId = setInterval(moveSnake, 100);
+    timerId = setInterval(iterateGame, 100);
 })
 
 setGridSize();
